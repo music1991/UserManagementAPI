f1c25ba45de0417e229f247b897dc7d3cfec3005
music1991 <sebastians201491@gmail.com>
2026-03-15 14:58:34 -0300

    enable swagger for prod

--- UserManagementAPI/Program.cs
+++ UserManagementAPI/Program.cs
@@ -110,11 +110,8 @@ var app = builder.Build();
 
 app.UseCors("AllowReact");
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
